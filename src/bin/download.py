4bc1d4f6b3f3c06f48b1418dad7b98c6309b893a
Leon Rauschning <rauschning@cip.ifi.lmu.de>
2023-10-24 12:04:47 +0200

    added process parallel download, made progress printing prettier

--- src/bin/download.py
+++ src/bin/download.py
@@ -3,6 +3,9 @@
 import requests
 import json
 import sys
+from multiprocessing import Pool
+from functools import partial
+from typing import List
 
 ENCODE_URL="https://www.encodeproject.org/"
 
@@ -15,13 +18,13 @@ def get_encode_json(enc_id: str) -> dict:
         print("ERROR in downloading accession metadata from ENCODE. Check your network connection!")
         raise e
 
-    if not 'accession' in enc_meta:
+    if not 'accession' in enc_meta or enc_meta['accession'] != enc_id:
         # there must have been an error downloading from ENCODE
         raise ValueError(f"ERROR: Found invalid metadata! Check the sample ID: {enc_id}")
     return enc_meta
 
 
-def download_from_encode(enc_id: str, work_dir: str='./') -> [str, dict]:
+def download(enc_id: str, work_dir: str ='./') -> tuple[str, dict]:
     """Downloads a data file from ENCODE. Returns a path to the downloaded file and a dictionary containing some metainformation.
     """
     enc_meta = get_encode_json(enc_id)
@@ -40,14 +43,18 @@ def download_from_encode(enc_id: str, work_dir: str='./') -> [str, dict]:
         progress = 0
         for chunk in requests.get(f"{ENCODE_URL}{enc_meta['href']}", allow_redirects=True).iter_content(chunk_size=chunksize):
             f.write(chunk)
-            progress += chunksize*100 // size
-            print(progress, "% done", "\033[0K\r", sep='')
+            print(int(progress), "% done", end="\033[0K\r", sep='')
             # stolen from https://www.baeldung.com/linux/echo-printf-overwrite-terminal-line
+            progress += chunksize*100 / size
 
     return path, meta
+
+def parallel_download(ids: List[str], work_dir: str ='./', max_workers: int =4) -> List[tuple[str, dict] ]:
+    with Pool(processes=max_workers) as p:
+        return p.map(partial(download, work_dir=work_dir), ids)
+
 
 if __name__ == "__main__":
     ids = sys.argv[1:]
     print(ids)
-    for i in ids:
-        print(download_from_encode(i))
+    print(parallel_download(ids))
